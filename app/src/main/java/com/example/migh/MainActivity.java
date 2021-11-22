544b325c5e2f26ca9d7c550b98e235816d0bb2bb
arsl <snnrehmani@gmail.com>
2021-11-22 23:58:05 +0500

    Menu in Guest Hand

--- app/src/main/java/com/example/migh/MainActivity.java
+++ app/src/main/java/com/example/migh/MainActivity.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btn,btn1;
+    EditText email;
+    EditText psw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        email= findViewById(R.id.login_uname);
+        psw = findViewById(R.id.login_pass);
+
         btn = (Button) findViewById(R.id.signup_btn1);
         btn1 = (Button) findViewById(R.id.login_btn1);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/migh/SignUpActivity.java
+++ app/src/main/java/com/example/migh/SignUpActivity.java
@@ -1,18 +1,79 @@
 package com.example.migh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
     EditText signup;
     EditText pass;
+    Button btn;
+    FirebaseAuth fAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signup=findViewById(R.id.signup_uname);
+        pass = findViewById(R.id.signup_pass);
+        btn =findViewById(R.id.signup_btn2);
+
+        fAuth =FirebaseAuth.getInstance();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=signup.getText().toString().trim();
+                String password=pass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    signup.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    pass.setError("password is Empty");
+                    return;
+                }
+                if (password.length()<5){
+                    pass.setError("please enter more then 5 digits");
+                    return;
+                }
+                //create database with firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this, "Error"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
+
+
+
+            }
+        });
     }
 }
